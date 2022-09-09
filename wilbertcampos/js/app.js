30408c3d70dc161931c079fc2e9e1cebc6fbd934
wilbert campos <wilbert@utponiente.edu.mx>
2022-09-09 17:41:21 -0500

    estructura

---
+++ wilbertcampos/js/app.js
@@ -0,0 +1,16 @@
+const showCoffees = () => {
+    let output = ""
+    coffees.forEach(
+      ({ name, image }) =>
+        (output += `
+                <div class="card">
+                  <img class="card--avatar" src=${image} />
+                  <h1 class="card--title">${name}</h1>
+                  <a class="card--link" href="#">Taste</a>
+                </div>
+                `)
+    )
+    container.innerHTML = output
+  }
+  
+  document.addEventListener("DOMContentLoaded", showCoffees)
